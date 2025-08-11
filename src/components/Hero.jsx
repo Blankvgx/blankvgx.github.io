da8c9f5004871a65e03b0edeebf065fdbd3d00d8
Blankvgx <markcg.villanueva@gmail.com>
2025-08-11 13:27:40 -0400

    Fixed, I do section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,13 +15,25 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">lohit</span>
+            <p className={`${styles.heroHeadText} text-white`}>
+              <Typewriter
+                options={{
+                  strings: ["Hello!", "Kumusta!", "Hola!"],
+                  autoStart: true,
+                  loop: true,
+                  loopCount: Infinity,
+                  deleteSpeed: "natural",
+                  pauseFor: 1000,
+                }}
+              />
+              I'm <span className="text-[#915EFF]">Mark</span>
+            </p>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I do
             <Typewriter
               options={{
-                strings: ["CyberSecurity", "Ai & ML", "NLP"],
+                strings: ["Game Development", "Databases", "Cloud", "Backend Development"],
                 autoStart: true,
                 loop: true,
                 loopCount: Infinity,
