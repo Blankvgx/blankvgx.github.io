99981af4ca9cea831b80317f06fdd5c5a4e79c6d
Blankvgx <markcg.villanueva@gmail.com>
2025-08-12 13:15:12 -0400

    Adding some changes to Website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const toggleResume = () => {
-    const resumeUrl = '/Resume.pdf';
+    const resumeUrl = '/MarkCedrickVillanueva_ITResume.pdf';
     window.open(resumeUrl);
   };
 
